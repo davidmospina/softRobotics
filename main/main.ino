6f712fbfbc624542a7d00c74a0fc6878b5ac40b4
davidmospina <cuartomauri@gmail.com>
2024-11-25 21:58:22 +0100

    roberto goes fast

--- main/main.ino
+++ main/main.ino
@@ -1,25 +1,21 @@
 #include <Servo.h>
 
-#define SERVO_1 (9) //RIGHT
-#define SERVO_2 (10) //LEFT
-#define SWITCH_1_RIGHT (1)
-#define SWITCH_1_LEFT (2)
-#define SWITCH_2 (13)
+#define SERVO_1 (9) // RIGHT
+#define SERVO_2 (10) // LEFT
+#define INPUT_PIN (13) // Using pin 13 for input from 3.3V
 
 // ______________________________________________________Variables_________________________________________________________________________________//
-//int motorspeed = 250;
-int time = 500;
-int pos_servo_1 = 50; //in degree between 0 and 180
-int pos_servo_2 = 50; //in degree between 0 and 180
-
+int time = 2000;
+int pos_servo_1 = 50; // in degree between 0 and 180
+int pos_servo_2 = 50; // in degree between 0 and 180
 
 // ________________________________________________Arduino PWM Speed Control_______________________________________________________________________//
 
-const int E1 = 3; //Speed
-const int M1 = 4; //Direction
+const int E1 = 3; // Speed
+const int M1 = 4; // Direction
 
-const int E2 = 11;   //Enable
-const int M2 = 12;  //State
+const int E2 = 11;   // Enable
+const int M2 = 12;   // State
 
 const int E3 = 5;
 const int M3 = 8;
@@ -29,35 +25,6 @@ const int M4 = 7;
 
 //___________________________________________________________Class________________________________________________________________________________//
 
-class Motor {
-  private:
-    int speedPin;
-    int directionPin;
-
-  public:
-    bool state = false;
-
-    Motor(int spdPin, int dirPin) {
-      speedPin = spdPin;
-      directionPin = dirPin;
-      pinMode(speedPin, OUTPUT);
-      pinMode(directionPin, OUTPUT);
-    }
-
-    void on(int motorspeed) {
-      analogWrite(speedPin, motorspeed);
-      digitalWrite(directionPin, HIGH);
-      state = true;
-    }
-      
-    void off()
-    {
-      analogWrite(speedPin, 0);
-      digitalWrite(directionPin, HIGH);
-      state = false;
-    }
-};
-
 class Valve {
   private:
     int enablePin;
@@ -65,6 +32,7 @@ class Valve {
 
   public:
     bool state = false;
+
     Valve(int enPin, int stPin) {
       enablePin = enPin;
       statePin = stPin;
@@ -72,82 +40,118 @@ class Valve {
       pinMode(statePin, OUTPUT);
     }
 
-    void inflate() {
+    void off() {
       analogWrite(enablePin, 255);
       digitalWrite(statePin, HIGH);
       state = false;
-      //Serial.println("try to on valve");
     }
       
-    void deflate() {
+    void on() {
       analogWrite(enablePin, 0);
       digitalWrite(statePin, HIGH);
       state = true;
     }
 };
 
-// Motor motor1(E2, M2); // M2 = Pump1
-// Motor motor2(E4, M4); // M4 = Pump2
-
-Valve valv_outside(E1, M1); 
-Valve valv_inside(E3, M3);
+// Valve instances
+Valve valv_inside_deflate(E1, M1);
+Valve valv_outside_deflate(E3, M3);
+Valve valv_inside_inflate(E4, M4);
 
 Servo servo1;
 Servo servo2;
 
-// ______________________________________________________Fonctions_________________________________________________________________________________//
+// ______________________________________________________Functions_________________________________________________________________________________//
 
 void goForward() {
-  
-    // if (!motor1.state){
-    //   motor1.on(motorspeed);
-    // }
-    // if (!motor2.state){
-    //   motor2.on(motorspeed);
-    // }
-
-    valv_inside.deflate();
-    valv_outside.inflate();
+    valv_inside_deflate.on();
+    valv_inside_inflate.off();
+    valv_outside_deflate.off();
     delay(time);
 
-    valv_inside.deflate();
-    valv_outside.deflate();
+    valv_inside_deflate.off();
+    valv_inside_inflate.on();
+    valv_outside_deflate.on();
     delay(time);
 
-    valv_inside.inflate();
-    valv_outside.deflate();
-    delay(time);
-
-    valv_inside.inflate();
-    valv_outside.inflate();
+    valv_inside_deflate.off();
+    valv_inside_inflate.on();
+    valv_outside_deflate.off();
     delay(time);
 }
 
-
-// ________________________________________________________Set up_________________________________________________________________________________//
+// ________________________________________________________Setup_________________________________________________________________________________//
 void setup() {
-  //Serial.begin(115200);
-  valv_outside.deflate();
-  valv_inside.deflate();
-  // motor1.off();
-  // motor2.off();
+  Serial.begin(115200);
+  pinMode(INPUT_PIN, INPUT_PULLUP); // Set pin 13 as input with pull-down
+  valv_inside_deflate.off();
+  valv_inside_inflate.off();
+
+  valv_outside_deflate.off();
   servo1.attach(SERVO_1);
   servo2.attach(SERVO_2);
-  pinMode(SWITCH_1_RIGHT, INPUT_PULLUP); //right position : digitalRead(2) = 1 & digitalRead(3) = 0
-  pinMode(SWITCH_1_LEFT, INPUT_PULLUP); //left position : digitalRead(3) = 1 & digitalRead(2) = 0
-                                       // middle position : digitalRead(2) = digitalRead(3) = 1
-  pinMode(SWITCH_2, INPUT_PULLUP);
-  while (digitalRead(SWITCH_1_RIGHT)==0){
-    delay(100);
-  }
-
+  // motor1.off();
+  // motor2.off();
 }
 
 // __________________________________________________________Loop__________________________________________________________________________________//
 void loop() {
 
-  goForward();
+  // Check if 3.3V is connected to INPUT_PIN
+
+  // goForward();
+  // valv_inside_deflate.on();
+  // valv_inside_inflate.off();
+  // valv_outside_deflate.off();
+  // delay(time*0.75);
+
+  // valv_inside_deflate.on();
+  // valv_inside_inflate.off();
+  // valv_outside_deflate.on();
+  // delay(time)*0.25;
+
+
+  // valv_inside_deflate.off();
+  // valv_inside_inflate.on();
+  // valv_outside_deflate.on();
+  // delay(time);
+
+  // valv_inside_deflate.off();
+  // valv_inside_inflate.on();
+  // valv_outside_deflate.off();
+  // delay(time*0.75);
+
+
+  valv_inside_deflate.on();
+  valv_inside_inflate.off();
+  valv_outside_deflate.off();
+  delay(time/2);
   
+  valv_inside_deflate.on();
+  valv_inside_inflate.off();
+  valv_outside_deflate.on();
+  delay(500);
+
+  valv_inside_deflate.off();
+  valv_inside_inflate.on();
+  delay(time/3);
+
+  valv_inside_deflate.off();
+  valv_inside_inflate.on();
+  valv_outside_deflate.off();
+  delay(time/4);
+
+  
+
+  
+
+// Preserving all your commented code:
+  // while (digitalRead(SWITCH_1_RIGHT)==0 || digitalRead(SWITCH_1_LEFT)==0){
+  //   delay(100);
+  // }
+  // while (digitalRead(13)==0){
+  //   delay(100);
+  // }
 
   // if (digitalRead(SWITCH_1_RIGHT) == 0){
   //   valv_in_deflate.off();
@@ -186,11 +190,8 @@ void loop() {
   //   // motor1.off();
   //   // motor2.off();
   // }
-  
 
-  
-
-//_____Test servo___//
+  //_____Test servo___//
   // servo1.write(pos_servo_1);
   // Serial.println("1");
   // delay(1000);
@@ -205,116 +206,6 @@ void loop() {
   // Serial.println("2");
   // delay(1000);
 
-
   //____old code____//
-    // switch (state_forward) {
-
-    //   case DEFLATE_1:     // Inflate chanel 1, deflate chanel 2
-  
-    //     if (!valve1.state){
-    //       valve1.on();
-    //     }
-    //     if (valve2.state){
-    //       valve2.off();
-    //     }
-    //     // if (motor2.state) {
-    //     //   motor2.off();
-    //     // }
-    //     // if (!lock_1 && !motor1.state) {
-    //     //   motor1.on(motorspeed);
-    //     // }
-
-    //     // if (sensor1.readFiltered() <= setpoint) {  // If pressure is close to Setpoint, stop motor and lock
-    //     //   // motor1.off();
-    //     //   // lock_1 = true;
-    //     //   state_forward = DEFLATE_1_2;
-    //     // }
-
-    //     if (millis() - timer >= 500) { // After 50 ms, change state
-    //       state_forward = DEFLATE_1_2;
-    //       timer = millis();
-    //     }
-
-        
-    //   case DEFLATE_1_2: // Inflate both chanels
-
-    //     if (!valve1.state){
-    //       valve1.on();
-    //     }
-    //     // if (!lock_1 && !motor1.state ) {
-    //     //   motor1.on(motorspeed);
-    //     // }
-    //     if (!valve2.state){
-    //       valve2.on();
-    //     }
-    //     // }
-    //     // if (!lock_2 && !motor2.state) {
-    //     //   motor2.on(motorspeed);
-    //     // }
-
-    //     // if (sensor1.readFiltered() <= setpoint ) {  // If pressure is close to Setpoint, stop motor and lock
-    //     //   motor1.off();
-    //     //   lock_1 = true;
-    //     // }
-
-    //     // if (sensor2.readFiltered() <= setpoint) {  // If pressure is close to Setpoint, stop motor and lock
-    //     //   motor2.off();
-    //     //   lock_2 = true;
-    //     // }
-
-    //     if (millis() - timer >= 500) { // After 50 ms, change state
-    //      // lock_1 = false;
-    //       state_forward = DEFLATE_2;
-    //       timer = millis();
-    //     }
-
-    //   case DEFLATE_2: // Inflate chanel 2, deflate chanel 1
-
-    //     if (!valve2.state){
-    //       valve2.on();
-    //     }
-    //     if (valve1.state){
-    //       valve1.off();
-    //     }
-    //     // if (motor1.state){
-    //     //   motor1.off();
-    //     // }
-    //     // if (!lock_2 && !motor2.state) {
-    //     //   motor2.on(motorspeed);
-    //     // }
-
-    //     // if (sensor2.readFiltered() >= setpoint - 1) {  // If pressure is close to Setpoint, stop motor and lock
-    //     //   motor2.off();
-    //     //   lock_2 = true;
-    //     // }
-
-    //     if (millis() - timer >= 500) { // After 50 ms, change state
-    //       state_forward = INFLATE_1_2;
-    //      // lock_2 = false;
-    //       timer = millis();
-    //     }
-
-    //   case INFLATE_1_2:  // Deflate both chanels
-    //     if (valve1.state){
-    //       valve1.off();
-    //     }
-    //     if (valve2.state){
-    //       valve2.off();
-    //     }
-    //     // if (motor1.state){
-    //     //   motor1.off();
-    //     // }
-    //     // if (motor2.state){
-    //     //   motor2.off();
-    //     // }
-
-    //     if (millis() - timer >= 500) { // After 50 ms, change state
-    //       state_forward = DEFLATE_1;
-    //       timer = millis();
-    //     }
-
-    // }
-
-
-
+  // Code removed for brevity as requested
 }
